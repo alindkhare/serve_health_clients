e019c7974aaed26d44873007ae4e8362c1783ea1
Alind Khare <alindkhare@gatech.edu>
2020-01-22 16:17:14 -0500

    Added persistent connection part2

--- patient_client.go
+++ patient_client.go
@@ -8,12 +8,12 @@ import (
 	"time"
 )
 
-func MakeRequest(url string, ch chan<- string, client http.DefaultClient) {
+func MakeRequest(url string, ch chan<- string, client *http.Client) {
 	start := time.Now()
 	resp, _ := client.Get(url)
 	secs := time.Since(start).Seconds()
-	body, _ := ioutil.ReadAll(resp.Body)
 	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
 	ch <- fmt.Sprintf("%.2f elapsed with response length: %s %s", secs, body, url)
 	
 }
@@ -22,7 +22,7 @@ func main() {
 	patient_name := os.Args[1]
 	ip := os.Args[2]
 	fmt.Println(patient_name,ip)
-	client := http.DefaultClient
+	client := &http.Client{}
 	ch := make(chan string)
 	address := "http://"+ip+"/hospital?patient_name="+patient_name+"&value=0.0&vtype=ECG"
 	fmt.Println(address)
