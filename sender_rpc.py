4ed66f0c22e46242e5badf1a8b0151afae6bb955
Satria Priambada <spriambada3@eris.cc.gatech.edu>
2020-01-27 17:20:35 -0500

    add client address from eris to pluto

--- sender_rpc.py
+++ sender_rpc.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import socket
+import os
 
 def fire_remote_patient(url, req_params):
     payload = {
@@ -12,6 +14,13 @@ def fire_remote_patient(url, req_params):
     print("{}".format(response))
 
 if __name__ == "__main__":
-    url = "http://localhost:4000/jsonrpc"
-    req_params = {"npatients":1, "serve_ip":"localhost", "serve_port":8000}
+    gw = os.popen("ip -4 route show default").read().split()
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((gw[2], 0))
+    IPv4addr = s.getsockname()[0]  #for where the server ray.serve() request will be executed
+    serve_port = 8000
+
+    url = "http://130.207.25.143:4000/jsonrpc" #for client address. In the experiment points to pluto
+    print("sending RPC request form IPv4 addr: {}".format(IPv4addr))
+    req_params = {"npatient":1, "serve_ip":IPv4addr, "serve_port":serve_port}
     fire_remote_patient(url, req_params)
